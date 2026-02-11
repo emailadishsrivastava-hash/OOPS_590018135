01a624e4b1b29da250c990f1851a97c72a0556f5
Adish Srivastava <email.adish.srivastava@gmail.com>
2026-02-11 11:43:49 +0530

    Create Var.java

---
+++ Var.java
@@ -0,0 +1,18 @@
+class Student {
+    String name;
+    int age;
+
+}
+
+public class Var {
+    public static void main(String[] args) {
+    Student s = new Student();
+    
+    s.name = "SHIVA TYAGI";
+    s.age = 19;
+
+    System.out.println(s.name);
+    System.out.println(s.age);
+    }
+    
+}
